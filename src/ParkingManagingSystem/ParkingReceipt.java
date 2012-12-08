59166b30c8a6272669e0c375beb3af686f0c024f
haiweizhang <deanzhang@163.com>
2012-12-08 23:03:37 +0800

    add ParkingReceipt class

---
+++ src/ParkingManagingSystem/ParkingReceipt.java
@@ -0,0 +1,11 @@
+package ParkingManagingSystem;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dean
+ * Date: 12-12-8
+ * Time: PM11:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class ParkingReceipt {
+}
