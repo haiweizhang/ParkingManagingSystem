b8d9897c2977cc251d592e0cb76fac430de5a759
haiweizhang <deanzhang@163.com>
2012-12-16 16:55:18 +0800

    add parking manager

---
+++ src/ParkingManagingSystem/ParkingManager.java
@@ -0,0 +1,13 @@
+package ParkingManagingSystem;
+
+import java.util.List;
+/**
+ * Created with IntelliJ IDEA.
+ * User: software
+ * Date: 12-12-16
+ * Time: 下午3:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class ParkingManager {
+    List<ParkingBoy> parkingBoyList;
+}
